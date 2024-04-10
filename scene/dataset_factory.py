61114f6ebb9f777962a7e74055a60f4623e3e1fa
Ben Kaye <kaye@login1.triton.cluster>
2024-04-10 12:01:17 +0100

    put the dataset locations in a yaml

--- scene/dataset_factory.py
+++ scene/dataset_factory.py
@@ -3,19 +3,19 @@ srn_failed, co3d_failed = False, False
 
 try:
     from .srn import SRNDataset
-except ImportError as e:
+except AssertionError as e:
     srn_failed = True
 try:
     from .co3d import CO3DDataset
-except ImportError as e:
+except AssertionError as e:
     co3d_failed = True
 
 def get_dataset(cfg, name):
     if cfg.data.category == "cars" or cfg.data.category == "chairs":
         if srn_failed:
-            raise ImportError('srn.py failed to import!')
+            raise ImportError('SRN dataset not found. Update env.yaml')
         return SRNDataset(cfg, name)
     elif cfg.data.category == "hydrants" or cfg.data.category == "teddybears":
         if co3d_failed:
-            raise ImportError('co3d.py failed to import!')
+            raise ImportError('CO3D dataset not found. Update env.yaml')
         return CO3DDataset(cfg, name)
