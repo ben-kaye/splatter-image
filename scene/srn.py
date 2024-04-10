12739e70e306027b4ce442bba048e97626b3dc4a
Ben Kaye <kaye@login1.triton.cluster>
2024-04-10 12:01:17 +0100

    put the dataset locations in a yaml

--- scene/srn.py
+++ scene/srn.py
@@ -9,8 +9,11 @@ from .dataset_readers import readCamerasFromTxt
 from utils.general_utils import PILtoTorch, matrix_to_quaternion
 from utils.graphics_utils import getWorld2View2, getProjectionMatrix, getView2World
 
-SHAPENET_DATASET_ROOT = None # Change this to your data directory
-assert SHAPENET_DATASET_ROOT is not None, "Update the location of the SRN Shapenet Dataset"
+import yaml
+mod_dir = os.path.dirname(os.path.realpath(__file__))
+SHAPENET_DATASET_ROOT = yaml.safe_load(open(f'{mod_dir}/../env.yaml', 'r'))['srn_source_dir']
+assert SHAPENET_DATASET_ROOT is not None, "Update the location of the SRN Shapenet Dataset in ./env.yaml"
+
 
 class SRNDataset(Dataset):
     def __init__(self, cfg,
@@ -39,9 +42,9 @@ class SRNDataset(Dataset):
 
         self.projection_matrix = getProjectionMatrix(
             znear=self.cfg.data.znear, zfar=self.cfg.data.zfar,
-            fovX=cfg.data.fov * 2 * np.pi / 360, 
-            fovY=cfg.data.fov * 2 * np.pi / 360).transpose(0,1)
-        
+            fovX=cfg.data.fov * 2 * np.pi / 360,
+            fovY=cfg.data.fov * 2 * np.pi / 360).transpose(0, 1)
+
         self.imgs_per_obj = self.cfg.opt.imgs_per_obj
 
         # in deterministic version the number of testing images
@@ -57,7 +60,7 @@ class SRNDataset(Dataset):
         return len(self.intrins)
 
     def load_example_id(self, example_id, intrin_path,
-                        trans = np.array([0.0, 0.0, 0.0]), scale=1.0):
+                        trans=np.array([0.0, 0.0, 0.0]), scale=1.0):
         dir_path = os.path.dirname(intrin_path)
         rgb_paths = sorted(glob.glob(os.path.join(dir_path, "rgb", "*")))
         pose_paths = sorted(glob.glob(os.path.join(dir_path, "pose", "*")))
@@ -77,30 +80,41 @@ class SRNDataset(Dataset):
             self.all_camera_centers[example_id] = []
             self.all_view_to_world_transforms[example_id] = []
 
-            cam_infos = readCamerasFromTxt(rgb_paths, pose_paths, [i for i in range(len(rgb_paths))])
+            cam_infos = readCamerasFromTxt(rgb_paths, pose_paths, [
+                                           i for i in range(len(rgb_paths))])
 
             for cam_info in cam_infos:
                 R = cam_info.R
                 T = cam_info.T
 
-                self.all_rgbs[example_id].append(PILtoTorch(cam_info.image, 
+                self.all_rgbs[example_id].append(PILtoTorch(cam_info.image,
                                                             (self.cfg.data.training_resolution, self.cfg.data.training_resolution)).clamp(0.0, 1.0)[:3, :, :])
 
-                world_view_transform = torch.tensor(getWorld2View2(R, T, trans, scale)).transpose(0, 1)
-                view_world_transform = torch.tensor(getView2World(R, T, trans, scale)).transpose(0, 1)
+                world_view_transform = torch.tensor(
+                    getWorld2View2(R, T, trans, scale)).transpose(0, 1)
+                view_world_transform = torch.tensor(
+                    getView2World(R, T, trans, scale)).transpose(0, 1)
 
-                full_proj_transform = (world_view_transform.unsqueeze(0).bmm(self.projection_matrix.unsqueeze(0))).squeeze(0)
+                full_proj_transform = (world_view_transform.unsqueeze(
+                    0).bmm(self.projection_matrix.unsqueeze(0))).squeeze(0)
                 camera_center = world_view_transform.inverse()[3, :3]
 
-                self.all_world_view_transforms[example_id].append(world_view_transform)
-                self.all_view_to_world_transforms[example_id].append(view_world_transform)
-                self.all_full_proj_transforms[example_id].append(full_proj_transform)
+                self.all_world_view_transforms[example_id].append(
+                    world_view_transform)
+                self.all_view_to_world_transforms[example_id].append(
+                    view_world_transform)
+                self.all_full_proj_transforms[example_id].append(
+                    full_proj_transform)
                 self.all_camera_centers[example_id].append(camera_center)
-            
-            self.all_world_view_transforms[example_id] = torch.stack(self.all_world_view_transforms[example_id])
-            self.all_view_to_world_transforms[example_id] = torch.stack(self.all_view_to_world_transforms[example_id])
-            self.all_full_proj_transforms[example_id] = torch.stack(self.all_full_proj_transforms[example_id])
-            self.all_camera_centers[example_id] = torch.stack(self.all_camera_centers[example_id])
+
+            self.all_world_view_transforms[example_id] = torch.stack(
+                self.all_world_view_transforms[example_id])
+            self.all_view_to_world_transforms[example_id] = torch.stack(
+                self.all_view_to_world_transforms[example_id])
+            self.all_full_proj_transforms[example_id] = torch.stack(
+                self.all_full_proj_transforms[example_id])
+            self.all_camera_centers[example_id] = torch.stack(
+                self.all_camera_centers[example_id])
             self.all_rgbs[example_id] = torch.stack(self.all_rgbs[example_id])
 
     def get_example_id(self, index):
@@ -109,24 +123,28 @@ class SRNDataset(Dataset):
         return example_id
 
     def make_poses_relative_to_first(self, images_and_camera_poses):
-        inverse_first_camera = images_and_camera_poses["world_view_transforms"][0].inverse().clone()
+        inverse_first_camera = images_and_camera_poses["world_view_transforms"][0].inverse(
+        ).clone()
         for c in range(images_and_camera_poses["world_view_transforms"].shape[0]):
             images_and_camera_poses["world_view_transforms"][c] = torch.bmm(
-                                                inverse_first_camera.unsqueeze(0),
-                                                images_and_camera_poses["world_view_transforms"][c].unsqueeze(0)).squeeze(0)
+                inverse_first_camera.unsqueeze(0),
+                images_and_camera_poses["world_view_transforms"][c].unsqueeze(0)).squeeze(0)
             images_and_camera_poses["view_to_world_transforms"][c] = torch.bmm(
-                                                images_and_camera_poses["view_to_world_transforms"][c].unsqueeze(0),
-                                                inverse_first_camera.inverse().unsqueeze(0)).squeeze(0)
+                images_and_camera_poses["view_to_world_transforms"][c].unsqueeze(
+                    0),
+                inverse_first_camera.inverse().unsqueeze(0)).squeeze(0)
             images_and_camera_poses["full_proj_transforms"][c] = torch.bmm(
-                                                inverse_first_camera.unsqueeze(0),
-                                                images_and_camera_poses["full_proj_transforms"][c].unsqueeze(0)).squeeze(0)
-            images_and_camera_poses["camera_centers"][c] = images_and_camera_poses["world_view_transforms"][c].inverse()[3, :3]
+                inverse_first_camera.unsqueeze(0),
+                images_and_camera_poses["full_proj_transforms"][c].unsqueeze(0)).squeeze(0)
+            images_and_camera_poses["camera_centers"][c] = images_and_camera_poses["world_view_transforms"][c].inverse()[
+                3, :3]
         return images_and_camera_poses
 
     def get_source_cw2wT(self, source_cameras_view_to_world):
         qs = []
         for c_idx in range(source_cameras_view_to_world.shape[0]):
-            qs.append(matrix_to_quaternion(source_cameras_view_to_world[c_idx, :3, :3].transpose(0, 1)))
+            qs.append(matrix_to_quaternion(
+                source_cameras_view_to_world[c_idx, :3, :3].transpose(0, 1)))
         return torch.stack(qs, dim=0)
 
     def __getitem__(self, index):
@@ -136,16 +154,17 @@ class SRNDataset(Dataset):
         self.load_example_id(example_id, intrin_path)
         if self.dataset_name == "train":
             frame_idxs = torch.randperm(
-                    len(self.all_rgbs[example_id])
-                    )[:self.imgs_per_obj]
+                len(self.all_rgbs[example_id])
+            )[:self.imgs_per_obj]
 
-            frame_idxs = torch.cat([frame_idxs[:self.cfg.data.input_images], frame_idxs], dim=0)
+            frame_idxs = torch.cat(
+                [frame_idxs[:self.cfg.data.input_images], frame_idxs], dim=0)
 
         else:
             input_idxs = self.test_input_idxs
-            
-            frame_idxs = torch.cat([torch.tensor(input_idxs), 
-                                    torch.tensor([i for i in range(251) if i not in input_idxs])], dim=0) 
+
+            frame_idxs = torch.cat([torch.tensor(input_idxs),
+                                    torch.tensor([i for i in range(251) if i not in input_idxs])], dim=0)
 
         images_and_camera_poses = {
             "gt_images": self.all_rgbs[example_id][frame_idxs].clone(),
@@ -155,7 +174,9 @@ class SRNDataset(Dataset):
             "camera_centers": self.all_camera_centers[example_id][frame_idxs]
         }
 
-        images_and_camera_poses = self.make_poses_relative_to_first(images_and_camera_poses)
-        images_and_camera_poses["source_cv2wT_quat"] = self.get_source_cw2wT(images_and_camera_poses["view_to_world_transforms"])
+        images_and_camera_poses = self.make_poses_relative_to_first(
+            images_and_camera_poses)
+        images_and_camera_poses["source_cv2wT_quat"] = self.get_source_cw2wT(
+            images_and_camera_poses["view_to_world_transforms"])
 
         return images_and_camera_poses
